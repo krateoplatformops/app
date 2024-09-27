0f6afd425eaf713572bacb121488767c888dd5cd
Diego Braga <diego.braga86@gmail.com>
2024-09-27 22:50:48 +0200

    feat: remove api call

--- src/components/Pages/Deployments/Deployment/CatchAll/CatchAll.jsx
+++ src/components/Pages/Deployments/Deployment/CatchAll/CatchAll.jsx
@@ -124,7 +124,7 @@ const CatchAll = ({ deploy, params, plugin }) => {
             plugin={pp}
             deploy={deploy}
             content={plugin.data[pKey]}
-          detailsCallHandler={detailsCallHandler}
+          // detailsCallHandler={detailsCallHandler}
           />
         )
       case 'codequality':
